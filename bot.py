e5af0620e66250d3fd15184ba737905f4f3c0e0e
Chase Hiatt <18chiattinamerica@gmail.com>
2022-04-15 12:56:07 -0600

    Added Usage tracking

--- bot.py
+++ bot.py
@@ -1,17 +1,59 @@
 from cachetools import LRUCache
 import discord
 import json
+import datetime
+from collections import deque
 
 from CodeNames import Game
 from CodeNames import save
 MAX_CONCURENT_GAMES = 500
+PERIOD_DURATION = 18000 #5 hour periods
+MAX_PERIODS_TRACK = 5
+
+class TimeEntry:
+
+    def __init__(self) -> None:
+        self.startTime = datetime.datetime.now()
+        self.endTime =  self.startTime + datetime.timedelta(seconds=PERIOD_DURATION)
+        self.actions = 0
+
+def timeToStr(currTime: datetime.datetime):
+    return currTime.strftime("%b %d %H:%M:%S")
+
+class Stastics:
+
+    def __init__(self) -> None:
+        self.usage = deque[TimeEntry]()
+        self.usage.append(TimeEntry())
+
+    def action(self):
+        top = self.usage[len(self.usage) - 1]
+        if datetime.datetime.now() > top.endTime:
+            ##Add new top
+            top = TimeEntry()
+            self.usage.append(top)
+            if len(self.usage) > MAX_PERIODS_TRACK:
+                self.usage.popleft()
+        top.actions = top.actions + 1
+
+    def getStats(self):
+        theStrs = []
+        for entry in self.usage:
+            endTime = entry.endTime
+            currStr = f"Period {timeToStr(entry.startTime)} - {timeToStr(endTime)}: {entry.actions}"
+            theStrs.append(currStr)
+        return "\n".join(theStrs)
+
 
 class MyClient(discord.Client):
     
     def __init__(self):
         super(). __init__()
         self.games = LRUCache(MAX_CONCURENT_GAMES)
-
+        self.stastics = Stastics()
+        
+    def trackAction(self):
+        self.stastics.action()
     
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -25,6 +67,7 @@ class MyClient(discord.Client):
             return
         text :str = message.content.lower()
         if text.startswith("#about"):
+            self.trackAction()
             responseLines = ["Start a new game with the #init command, for example: ",
                        "```#init cheese broccoli shoe glove hand steak```",
                        "Remove words from your current game using #remove, for example:",
@@ -34,9 +77,10 @@ class MyClient(discord.Client):
             await message.channel.send("\n".join(responseLines))
 
         if text.startswith('#init'):
+            self.trackAction()
             words = text.split(' ')[1:]
-            if len(words) < 4:
-                await message.channel.send("Sorry, please enter at least 5 words for analysis")
+            if len(words) < 2:
+                await message.channel.send("Sorry, please enter at least 2 words for analysis")
                 return
             if len(words) > 50:
                 await message.channel.send("Sorry, max game size is 50")
@@ -48,6 +92,7 @@ class MyClient(discord.Client):
             await message.channel.send('Inited')
         
         if text.startswith("#guess"):
+            self.trackAction()
             try:
                 segments = text.split(' ')
                 count = None
@@ -68,6 +113,7 @@ class MyClient(discord.Client):
                 await message.channel.send(f"An error occured while processing your request: {e.with_traceback}")
         
         if text.startswith("#reduce"):
+            self.trackAction()
             game = self.games.get(str(user))
             if not game:
                 await message.channel.send("Please start a game first")
@@ -76,6 +122,10 @@ class MyClient(discord.Client):
             newwords = list(filter(lambda x : x not in toReduce, game.wordsInPlay))
             game.setWords(newwords)
             await message.channel.send(f"@{str(user)}\nReduced successfully, new word list: {' '.join(newwords)}"  )
+
+        if text.startswith("#stats"):
+            response = self.stastics.getStats()
+            await message.channel.send(response)
 
 def envSetup():
     print("Please create JSON file .env with contents similar to\n", json.dumps({"discordkey":"1234123412341234"}))
