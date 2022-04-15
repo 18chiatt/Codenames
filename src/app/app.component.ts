65bd628f6744bb622a5091c4964bce4068913a93
Chase Hiatt <18chiattinamerica@gmail.com>
2022-04-15 01:33:30 -0600

    Added re-try functionality if displaying PDF fails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { Subject, timer } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = "codenames-app";
+	@ViewChild("#iFrame") myFrame!: HTMLElement;
+	pulse = false;
+	showDummy = true;
+	loaded = new Subject();
 
 	constructor(private _dialog: MatDialog) {}
+
+	ngOnInit(): void {
+		timer(2000, 1000)
+			.pipe(takeUntil(this.loaded))
+			.subscribe(() => {
+				console.log("Doing");
+				this.pulse = true;
+				setTimeout(() => {
+					this.pulse = false;
+				});
+			});
+	}
+
+	loadedSuccessfully() {
+		this.loaded.next();
+		this.showDummy = false;
+	}
 }
